9b7d0fd2c335044e09afc65a470527a5c1ed47d5
chuyouchia <58682208+chuyouchia@users.noreply.github.com>
2020-12-27 16:06:39 +0800

    Create AR foundation intro (Fully Functional Mobile AR)

---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectSpawned;
+    private PlacementIndicator placementIndicator;
+
+    void Start()
+    {
+        placementIndicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(objectSpawned, 
+                placementIndicator.transform.position, placementIndicator.transform.rotation);
+        }
+    }
+}
---
+++ Assets/PlacementIndicator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicator : MonoBehaviour
+{
+    private ARRaycastManager rayCastManager;
+    private GameObject visual; //plane object created with the ARPlacementIndicator
+
+    void Start ()
+    {
+        //get all components from scene
+        rayCastManager = FindObjectOfType<ARRaycastManager>();
+        visual = transform.GetChild(0).gameObject;
+
+
+        //disable the visual on initiation
+        visual.SetActive(false);
+    }
+
+    private void Update()
+    {
+        //shoot a raycast from the screen
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayCastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        //if we hit the ar plane, update the position and rotation of the AR object
+        if (hits.Count > 0)
+        {
+            //set the position and rotation of the game object based on hits
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            //set the visual as active if not already active
+            if (!visual.activeInHierarchy)
+            {
+                visual.SetActive(true);
+            }
+        }
+    }
+}
